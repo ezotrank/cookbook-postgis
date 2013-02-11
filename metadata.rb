a4b76748467199a548d3b35ae5caecdaa79ac5b9
Maxim Kremenev <ezotrank@gmail.com>
2013-02-11 11:52:41 +0300

    init commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name "postgis"
+
+maintainer       "Maxim Kremenev"
+maintainer_email "ezo@kremenev.com"
+license          "All rights reserved"
+description      "Install postgis"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
